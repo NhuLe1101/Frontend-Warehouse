cc031af11449c430aa2823fb7759c04dfdca5eaa
Thieu Viet Hoang <thieuhoangent@gmail.com>
2024-07-23 23:53:12 +0700

    thieuhoang up navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css'; 
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  }
+
   return (
-    <div>
-      Navbar
+    <div className="navbar">
+      <div className="logo">
+        <Link to="/">Logo</Link>
+      </div>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <img src="icon_menu.png" alt="" />
+      </div>
+      <ul className={`nav-links ${showMenu ? 'active' : ''}`}>
+        <li>
+          <Link to="/">Trang chủ</Link>
+        </li>
+        <li>
+          <Link to="/">Booking</Link>
+        </li>
+        <li>
+          <Link to="/binpacking">Sắp xếp</Link>
+        </li>
+        <li>
+          <Link to="/">Kho hàng</Link>
+        </li>
+        <li>
+          <Link to="/">Đăng nhập</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
