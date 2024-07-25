56c2049ad57bf99798a10c85b102354ba184ee48
Thieu Viet Hoang <thieuhoangent@gmail.com>
2024-07-26 00:40:31 +0700

    thieuhoang up footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './home.css'; 
 
 const Home = () => {
   return (
-    <div>
-      HomePage
+    <div className='home'>
+      <p>HOME PAGE</p>  
     </div>
   )
 }
