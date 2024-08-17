ea9c0b002bc0b637e9963c246ef92b07abffc044
Thieu Viet Hoang <thieuhoangent@gmail.com>
2024-08-17 22:54:58 +0700

    sua navbar: them gach chan vao the a khi hover, active

--- src/pages/booking/Booking.jsx
+++ src/pages/booking/Booking.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Booking = () => {
   return (
     <div className='booking' style={{marginTop:'56px'}}>
-      <p>BOOKING PAGE</p>  
+      <p>BOOKING PAGE</p>
     </div>
   )
 }
