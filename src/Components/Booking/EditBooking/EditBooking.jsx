a95661d5dfd36f40fe00b96890ee749f715c7a82
Thieu Viet Hoang <thieuhoangent@gmail.com>
2024-11-13 17:45:55 +0700

    fix giao dien chi tiet booking, san pham

--- src/Components/Booking/EditBooking/EditBooking.jsx
+++ src/Components/Booking/EditBooking/EditBooking.jsx
@@ -1,132 +1,142 @@
-import React, { useState, useEffect } from 'react';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import './editbooking.css';
-import BookingService from '../../../api/booking';
-import Swal from 'sweetalert2';
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import "./editbooking.css";
+import BookingService from "../../../api/booking";
+import Swal from "sweetalert2";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiDialogContent-root': {
-        padding: theme.spacing(2),
-    },
-    '& .MuiDialogActions-root': {
-        padding: theme.spacing(1),
-    },
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
 }));
 
-
 const EditBooking = ({ booking, onClose, onOpen }) => {
-    const [formData, setFormData] = useState(booking);
+  const [formData, setFormData] = useState(booking);
 
-    const AlertSuccess = () => {
-        Swal.fire({
-            icon: "success",
-            text: "Cập nhật thành công!"
-        }).then((result) => {
-            window.location.reload();
-        });
-    };
+  const AlertSuccess = () => {
+    Swal.fire({
+      icon: "success",
+      text: "Cập nhật thành công!",
+    }).then((result) => {
+      window.location.reload();
+    });
+  };
 
-    useEffect(() => {
-        setFormData(booking);
-    }, [booking]);
+  useEffect(() => {
+    setFormData(booking);
+  }, [booking]);
 
-    const handleInputChange = async (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handleInputChange = async (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleSaveClick = async () => {
-        try {
-            //console.log(formData);
-            await BookingService.updateBooking(formData);
-            //setSaveStatus('success');
-            AlertSuccess();
-        } catch (error) {
-            //setSaveStatus('error');
-            console.error("Có lỗi xảy ra khi upload dữ liệu:", error);
-        }
-    };
-    if (!booking) return null;
+  const handleSaveClick = async () => {
+    try {
+      //console.log(formData);
+      await BookingService.updateBooking(formData);
+      //setSaveStatus('success');
+      AlertSuccess();
+    } catch (error) {
+      //setSaveStatus('error');
+      console.error("Có lỗi xảy ra khi upload dữ liệu:", error);
+    }
+  };
 
-    return (
-        <BootstrapDialog
-            onClose={onClose}
-            aria-labelledby="customized-dialog-title"
-            open={onOpen}
-            style={{zIndex: '0'}}
-        >
-            <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-                Chi tiết Booking
-            </DialogTitle>
-            <IconButton
-                aria-label="close"
-                onClick={onClose}
-                sx={(theme) => ({
-                    position: 'absolute',
-                    right: 8,
-                    top: 8,
-                    color: theme.palette.grey[500],
-                })}
-            >
-                <CloseIcon />
-            </IconButton>
-            <DialogContent dividers style={{width:'360px'}}>
-                <form className='formEditBk'>
-                    <label>
-                        ID:
-                        <input
-                            type="text"
-                            name="id"
-                            value={formData.id}
-                            onChange={handleInputChange}
-                            readOnly
-                            disabled
-                        />
-                    </label>
-                    <label>
-                        Tên:
-                        <input
-                            type="text"
-                            name="customerName"
-                            value={formData.customerName}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Email:
-                        <input
-                            type="email"
-                            name="customerEmail"
-                            value={formData.customerEmail}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Điện thoại:
-                        <input
-                            type="text"
-                            name="numberphone"
-                            value={formData.numberphone}
-                            onChange={handleInputChange}
-                        />
-                    </label>
+  if (!booking) return null;
 
-                </form>
-            </DialogContent>
-            <DialogActions>
-                <Button variant="contained" onClick={handleSaveClick}>
-                    LƯU
-                </Button>
-            </DialogActions>
-        </BootstrapDialog>
-    );
+  function formatId(id) {
+    if (id < 10) {
+      return "BK000" + id;
+    } else if (id < 100) {
+      return "BK00" + id;
+    } else if (id < 1000) {
+      return "BK0" + id;
+    } else {
+      return "BK" + id;
+    }
+  }
+  return (
+    <BootstrapDialog
+      onClose={onClose}
+      aria-labelledby="customized-dialog-title"
+      open={onOpen}
+      style={{ zIndex: "0" }}
+    >
+      <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+        Chi tiết Booking
+      </DialogTitle>
+      <IconButton
+        aria-label="close"
+        onClick={onClose}
+        sx={(theme) => ({
+          position: "absolute",
+          right: 8,
+          top: 8,
+          color: theme.palette.grey[500],
+        })}
+      >
+        <CloseIcon />
+      </IconButton>
+      <DialogContent dividers style={{ width: "360px" }}>
+        <form className="formEditBk">
+          <label>
+            ID:
+            <input
+              type="text"
+              name="id"
+              value={formatId(formData.id)}
+              onChange={handleInputChange}
+              readOnly
+              disabled
+            />
+          </label>
+          <label>
+            Tên:
+            <input
+              type="text"
+              name="customerName"
+              value={formData.customerName}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="customerEmail"
+              value={formData.customerEmail}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Điện thoại:
+            <input
+              type="text"
+              name="numberphone"
+              value={formData.numberphone}
+              onChange={handleInputChange}
+            />
+          </label>
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" onClick={handleSaveClick}>
+          LƯU
+        </Button>
+      </DialogActions>
+    </BootstrapDialog>
+  );
 };
 
 export default EditBooking;
--- src/Components/Product/EditProduct/EditProduct.jsx
+++ src/Components/Product/EditProduct/EditProduct.jsx
@@ -1,178 +1,193 @@
-import React, { useState, useEffect } from 'react';
-import './editproduct.css';
-import ProductService from '../../../api/product';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import Swal from 'sweetalert2';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import "./editproduct.css";
+import ProductService from "../../../api/product";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Swal from "sweetalert2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const EditProduct = ({ product, onClose }) => {
-    const [formData, setFormData] = useState(product);
-    const [saveStatus, setSaveStatus] = useState(null);
+  const [formData, setFormData] = useState(product);
+  const [saveStatus, setSaveStatus] = useState(null);
 
-    const [showReferenceNo, setShowReferenceNo] = useState(false);
-    const [deliveryValue, setDeliveryValue] = useState('');
+  const [showReferenceNo, setShowReferenceNo] = useState(false);
+  const [deliveryValue, setDeliveryValue] = useState("");
 
-    const toggleDeliveryVisibility = () => {
-        setShowReferenceNo(!showReferenceNo);
-    };
+  const toggleDeliveryVisibility = () => {
+    setShowReferenceNo(!showReferenceNo);
+  };
 
-    useEffect(() => {
-        setFormData(product);
-    }, [product]);
+  useEffect(() => {
+    setFormData(product);
+  }, [product]);
 
-    const handleInputChange = async (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handleInputChange = async (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const AlertSuccess = () => {
-        Swal.fire({
-            icon: "success",
-            text: "Cập nhật thành công!"
-        }).then((result) => {
-            window.location.reload();
-        });
-    };
+  const AlertSuccess = () => {
+    Swal.fire({
+      icon: "success",
+      text: "Cập nhật thành công!",
+    }).then((result) => {
+      window.location.reload();
+    });
+  };
 
-    const handleSaveClick = async () => {
-        try {
-            console.log(formData);
-            const message = await ProductService.updateProduct(formData);
-            //alert(message);
-            setSaveStatus('success');
-            //window.location.reload();
-            AlertSuccess();
-        } catch (error) {
-            setSaveStatus('error');
-            console.error("Có lỗi xảy ra khi upload dữ liệu:", error);
-        }
-    };
-    if (!product) return null;
+  const handleSaveClick = async () => {
+    try {
+      console.log(formData);
+      const message = await ProductService.updateProduct(formData);
+      //alert(message);
+      setSaveStatus("success");
+      //window.location.reload();
+      AlertSuccess();
+    } catch (error) {
+      setSaveStatus("error");
+      console.error("Có lỗi xảy ra khi upload dữ liệu:", error);
+    }
+  };
+  if (!product) return null;
 
-    return (
-        <div className="popup-pd">
-            <div className="popup-content">
-                <h2>THÔNG TIN SẢN PHẨM</h2>
-                <form className='formEditPd'>
-                    <label>
-                        ID:
-                        <input
-                            type="text"
-                            name="stt"
-                            value={formData.itemId}
-                            readOnly
-                            disabled
-                            style={{ border: 'none' }}
-                        />
-                    </label>
-                    <label>
-                        Booking:
-                        <input
-                            type="text"
-                            name="booking"
-                            value={formData.booking.id}
-                            readOnly
-                            disabled
-                            style={{ border: 'none' }}
-                        />
-                    </label>
-                    <label>
-                        Hình ảnh:
-                        <img src={formData.image} alt="" width={50} />
-                    </label>
-                    <label>
-                        Tên:
-                        <input
-                            type="text"
-                            name="name"
-                            value={formData.name}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Số lượng:
-                        <input
-                            type="number"
-                            name="quantity"
-                            value={formData.quantity}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Trạng thái:
-                        <select name="status" id="" onChange={handleInputChange}>
-                            <option value="Đang lưu kho">Đang lưu kho</option>
-                            <option value="Đã xuất kho">Đã xuất kho</option>
-                            <option value="Đã huỷ">Đã huỷ</option>
-                        </select>
-                    </label>
-                    <label>
-                        Ngày xuất:
-                        <input
-                            type="date"
-                            name="checkout"
-                            value={formData.checkout}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Ngày nhập:
-                        <input
-                            type="date"
-                            name="checkin"
-                            value={formData.checkin}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Cân nặng (g):
-                        <input
-                            type="text"
-                            name="weight"
-                            value={formData.weight}
-                            onChange={handleInputChange}
-                        />
+  function formatId(id) {
+    if (id < 10) {
+      return "SP000" + id;
+    } else if (id < 100) {
+      return "SP00" + id;
+    } else if (id < 1000) {
+      return "SP0" + id;
+    } else {
+      return "SP" + id;
+    }
+  }
 
-                    </label>
-                    <label>
-                        Vận chuyển:
-                        <input
-                            type='text'
-                            name="delivery"
-                            value={formData.delivery}
-                            onChange={handleInputChange}
-                            readOnly
-                            disabled
-                        />
-                    </label>
-                    <label>
-                    </label>
-                    <label>
-                        Mã xác nhận:
-                        <input
-                            type={showReferenceNo ? 'text' : 'password'}
-                            name="referenceNo"
-                            value={formData.booking.referenceNo}
-                            readOnly
-                            disabled
-                        />
-                        <span style={{position:'absolute', right:'40px'}} className="input-group-text" onClick={toggleDeliveryVisibility}>
-                            <FontAwesomeIcon icon={showReferenceNo ? faEyeSlash : faEye} />
-                        </span>
-                    </label>
-
-
-                </form>
-                <div className="popup-actions">
-                    <button className='btn-save-edit-bk' onClick={handleSaveClick}>Lưu</button>
-                    <button className='btn-cancel-edit-bk' onClick={onClose}><CloseIcon /></button>
-
-                </div>
-            </div>
+  return (
+    <div className="popup-pd">
+      <div className="popup-content">
+        <h2>THÔNG TIN SẢN PHẨM</h2>
+        <form className="formEditPd">
+          <label>
+            ID:
+            <input
+              type="text"
+              name="stt"
+              value={formatId(formData.itemId)}
+              readOnly
+              disabled
+              style={{ border: "none" }}
+            />
+          </label>
+          <label>
+            Booking:
+            <input
+              type="text"
+              name="booking"
+              value={formData.booking.id}
+              readOnly
+              disabled
+              style={{ border: "none" }}
+            />
+          </label>
+          <label>
+            Hình ảnh:
+            <img src={formData.image} alt="" width={50} />
+          </label>
+          <label>
+            Tên:
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Số lượng:
+            <input
+              type="number"
+              name="quantity"
+              value={formData.quantity}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Trạng thái:
+            {/* <select name="status" id="" onChange={handleInputChange} disabled>
+              <option value="Đang lưu kho">Đang lưu kho</option>
+              <option value="Đã xuất kho">Đã xuất kho</option>
+              <option value="Đã huỷ">Đã huỷ</option>
+            </select> */}
+            <input type="text" name="status" value={formData.status} disabled />
+          </label>
+          <label>
+            Ngày xuất:
+            <input
+              type="date"
+              name="checkout"
+              value={formData.checkout}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Ngày nhập:
+            <input
+              type="date"
+              name="checkin"
+              value={formData.checkin}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Cân nặng (g):
+            <input
+              type="text"
+              name="weight"
+              value={formData.weight}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Vận chuyển:
+            <input
+              type="text"
+              name="delivery"
+              value={formData.delivery}
+              onChange={handleInputChange}
+              readOnly
+            />
+          </label>
+          <label></label>
+          <label>
+            Mã xác nhận:
+            <input
+              type={showReferenceNo ? "text" : "password"}
+              name="referenceNo"
+              value={formData.booking.referenceNo}
+              readOnly
+              disabled
+            />
+            <span
+              style={{ position: "absolute", right: "40px" }}
+              className="input-group-text"
+              onClick={toggleDeliveryVisibility}
+            >
+              <FontAwesomeIcon icon={showReferenceNo ? faEyeSlash : faEye} />
+            </span>
+          </label>
+        </form>
+        <div className="popup-actions">
+          <button className="btn-save-edit-bk" onClick={handleSaveClick}>
+            Lưu
+          </button>
+          <button className="btn-cancel-edit-bk" onClick={onClose}>
+            <CloseIcon />
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default EditProduct;
