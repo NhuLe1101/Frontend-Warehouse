d82bd2561eeec036131b69df8e953175ff23b3d2
NhuLe1101 <anophen011@gmail.com>
2024-11-22 02:26:20 +0700

    test deploy

--- src/api/compartment.js
+++ src/api/compartment.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/api/compartments';
+// const API_URL = 'http://localhost:8080/api/compartments';
+const API_URL = `${process.env.REACT_APP_API_URL}/api/compartments/`;
 
 // Hàm thêm item vào compartment
 const addItemToCompartment = (compartmentId, itemId, quantity) => {
--- src/api/product.js
+++ src/api/product.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/product/";
+// const API_URL = "http://localhost:8080/api/product/";
+const API_URL = `${process.env.REACT_APP_API_URL}/api/product/`;
 
 const getAllProducts = () => {
   return axios
